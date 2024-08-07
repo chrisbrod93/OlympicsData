34b22c48f8691d8b5715ed3818a55ac5e19549a3
chrisbrod93 <137168685+chrisbrod93@users.noreply.github.com>
2024-08-07 18:07:53 +0100

    Update CreateMasterFile.py

--- CreateMasterFile.py
+++ CreateMasterFile.py
@@ -23,3 +23,15 @@ total_pop = pd.merge(df,df1, on='Country')
 pop_gdp = pd.merge(total_pop,df2, on='Country')
 
 
+
+### Creating a % Change in Population DataFrame for Reference 
+
+only_population_df = total_pop[['Country'] + total_pop.filter(like='Population',axis=1).columns.tolist()]
+only_population_df.set_index('Country', inplace=True)
+
+total_pop_pct_change = only_population_df.pct_change(axis=1) * 100
+total_pop_pct_change.columns = [f'{year}% Change' for year in total_pop_pct_change.columns]
+total_pop_pct_change.reset_index(inplace=True)
+total_pop_pct_change.filter(like='Population 1960% Change',axis=1)
+total_pop_pct_change
+
